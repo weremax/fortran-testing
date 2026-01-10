5283249609c244d47bc4d1c838c0822bdeeabd04
weremax <hardcode45@yahoo.com>
2026-01-10 09:06:27 -0500

    updated user_input in order to handle bad data entered

--- user_input.f90
+++ user_input.f90
@@ -5,9 +5,18 @@ program user_input
     character(len=20) :: int_as_string
     character(len=20) :: hometown
     character(len=20) :: name
+    integer :: status
 
-    print *, "What's your name, age and where you live?"
-    read(*, *) name, age, hometown
+    name = "UNKNOWN"
+    age = 0
+    hometown = "UNKNOWN"
+
+    do
+        print *, "What's your name, age and where you live?"
+        read(*, *, iostat=status) name, age, hometown
+        if (status == 0) exit
+        print *, "Sorry, I didn't understand that"
+    end do
 
     print *, "Hello, " // trim(name) // "!"
     write(int_as_string, '(I0)') age
